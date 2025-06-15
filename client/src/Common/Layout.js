9becf094b391f1a436171a4ac075d429013acbac
Avigail3648 <Avigail3648@gmail.com>
2025-06-15 18:07:59 +0300

    Create Layout.js

--- client/src/Common/Layout.js
+++ client/src/Common/Layout.js
@@ -1,21 +1,69 @@
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 const Layout = () => {
-    const token = localStorage.getItem("userToken")
+  const token = localStorage.getItem("userToken")
 
-    return <div className="page">
-        <header>
-            <Link className="linksHeader" to="/register">register </Link>&nbsp;
-            <Link className="linksHeader" to="/login">login  </Link>&nbsp;
-            <Link className="linksHeader" to="/products">products </Link>&nbsp;
-            <Link className="linksHeader" to="/AdminProduct">Product-Management </Link>&nbsp;
-            <Link className="linksHeader" to="/baskets">basket </Link>&nbsp;
-            {token ?
-                <Link className="linksHeader" to="/logout">logout </Link> : <></>}
-        </header>
-        <main >
-            <Outlet />
-        </main>
-    </div>
+  return <div className="page">
+    <header>
+      <NavLink
+        to="/register"
+        className={({ isActive }) =>
+          isActive ? "linksHeader active" : "linksHeader"
+        }
+      >
+        register
+      </NavLink>
+      &nbsp;
+      <NavLink
+        to="/login"
+        className={({ isActive }) =>
+          isActive ? "linksHeader active" : "linksHeader"
+        }
+      >
+        login
+      </NavLink>
+      &nbsp;
+      <NavLink
+        to="/products"
+        className={({ isActive }) =>
+          isActive ? "linksHeader active" : "linksHeader"
+        }
+      >
+        products
+      </NavLink>
+      &nbsp;
+      <NavLink
+        to="/AdminProduct"
+        className={({ isActive }) =>
+          isActive ? "linksHeader active" : "linksHeader"
+        }
+      >
+        Product-Management
+      </NavLink>
+      &nbsp;
+      <NavLink
+        to="/baskets"
+        className={({ isActive }) =>
+          isActive ? "linksHeader active" : "linksHeader"
+        }
+      >
+        basket
+      </NavLink>
+      &nbsp;
+      {token && (
+        <NavLink
+          to="/logout"
+          className={({ isActive }) =>
+            isActive ? "linksHeader active" : "linksHeader"
+          }
+        >
+          logout
+        </NavLink>
+      )}
+    </header>
+    <main >
+      <Outlet />
+    </main>
+  </div>
 }
 export default Layout
